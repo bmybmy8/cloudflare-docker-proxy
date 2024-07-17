813646e7fcd585abf4b9160162ffc4a8df5a807e
bmybmy8 <63861696+bmybmy8@users.noreply.github.com>
2024-07-17 18:30:10 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.bmybmy.chat": dockerHub,
+  "quay.bmybmy.chat": "https://quay.io",
+  "gcr.bmybmy.chat": "https://gcr.io",
+  "k8s-gcr.bmybmy.chat": "https://k8s.gcr.io",
+  "k8s.bmybmy.chat": "https://registry.k8s.io",
+  "ghcr.bmybmy.chat": "https://ghcr.io",
+  "cloudsmith.bmybmy.chat": "https://docker.cloudsmith.io",
+  "ecr.bmybmy.chat": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.bmybmy.chat": dockerHub,
 };
 
 function routeByHosts(host) {
